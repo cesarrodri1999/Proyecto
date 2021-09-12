91ddac52da4acd85811e95cf95173c399e2fb026
César Rodríguez <cesarrodriguezinfor@gmail.com>
2021-09-12 15:33:32 +0200

    Menu Movile

--- head.php
+++ head.php
@@ -3,9 +3,29 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<title>Document</title>
 </head>
 <body>
-	<section>
-		<p>Cabezera</p>
+	<section class="cabecera">
+		<h3>Looking for <br> a Wallpaper</h3>
+		<ul>
+			<li>
+				<input type="text" name="buscar" value="">
+				<span id="ico" class="fa fa-search"></span>
+			</li>
+			<li>
+				<div class="menu">
+					<input type="checkbox" id="check-menu">
+					<label for="check-menu">
+						<div class="icono-menu">
+							<div class="barra-menu"></div>
+							<div class="barra-menu"></div>
+							<div class="barra-menu"></div>
+						</div>
+					</label>
+				</div>
+			</li>
+		</ul>
 	</section>
